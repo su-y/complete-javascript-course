397fa461fe2cc79c3c6ce1f8d9fba3756fe7623b
Aljoscha <Sokolov>
2019-05-24 17:00:35 +0300

    tha object task

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,32 +1,68 @@
-var john = {
-    name: `John`,
-    age: 26,
-    job: `teacher`,
-    presentation: function(style, timeOfDay) {
-        if (style === `formal`) {
-            console.log(`Good ${timeOfDay} ladies and gentelmen! I\'m a ${this.job} and I\'m ${this.age} years old.`);
-            
-        } else if (style = `friendly`) {
-            console.log(`Hey sup. I\'m a ${this.job} and I\'m ${this.age} years old.`);
-            
-        }
-    }
-}
+/*
+--- Let's build a fun quiz game in the console! ---
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
 
 
-var emily = {
-    name: `Emily`,
-    age: 35,
-    job: `designer`,
-}
+// Question constrcutor
+
+let Question = function (question, answers, rightAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.rightAnswer = rightAnswer;
+    this.questionArray = function() {
+        questions.push(this);
+    };
+};
+
+let questions = [];
 
 
 
 
-john.presentation(`formal`, `morning`);
 
-john.presentation.call(emily, `friendly`, `afternoon`);
+questionOne = new Question('Whats the design systems color?',['Yellow', 'Blue', 'Red', 'Tediuos'], 2);
+console.log(questionOne);
 
-let johnFriendly = john.presentation.bind(john, `friendly`);
+questionOne.questionArray();
+console.log(questions);
 
-johnFriendly(`morning`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+--- Expert level ---
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
